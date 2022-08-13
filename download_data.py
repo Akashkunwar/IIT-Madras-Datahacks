7c3ff988e00e1bce35fd389377062becc0a12652
Akash Kumar <gnspda@gmail.com>
2022-08-13 19:41:37 +0530

    updated

--- download_data.py
+++ download_data.py
@@ -1,21 +1,6 @@
-# ̥
-# # !pip install yfinance
-# # importing liberary
-# from datetime import datetime
-# import yfinance as yf
-# import matplotlib.pyplot as plt
-
-# # Downloading Data
-# df = yf.download('RELIANCE.NS')
-
-# # Plotting graph 
-# plt.figure(figsize = (20,10))
-# plt.title('Price ')
-# plt.plot(df['Open'])
-# plt.show()
-# print(df)
-
 import functions as fn
-fn.get_data('RELIANCE.NS')
+fn.get_data('ZOMATO.NS')
 fn.show_data()
-fn.plot_data()
+# fn.plot_data()̥
+
+print(fn.df)
