591239de10cf34b000cd06c75a16461641ca946d
Akash Kumar <gnspda@gmail.com>
2022-08-14 05:33:33 +0530

    Update app.py

---
+++ app.py
@@ -0,0 +1,19 @@
+import yfinance as yf
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+from datetime import date
+from sklearn.preprocessing import MinMaxScaler
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.layers import LSTM
+import tensorflow as tf
+import math
+from sklearn.metrics import mean_squared_error
+import streamlit as st
+
+start = "2015-01-01"
+today = date.today().strftime("%Y-%m-%d")
+
+st.title('Forecast of Stock Market')
+
